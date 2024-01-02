dbcba578386d59ab3940745287b923344ff71267
adibmansuri511 <adibmansuri.developer@gmail.com>
2024-01-02 15:52:33 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,31 +4,29 @@ function factorial() {
     // Take input from the user
     const number = Number(document.getElementById("number").value);
 
-
     let answer = "";
 
     // checking if number is negative
     if (number < 0) {
         answer = 'Error! Factorial for negative number does not exist.';
-        console.log(answer);
-        document.querySelector('#h4').innerHTML = answer;
     }
 
     // if number is 0
     else if (number === 0) {
         answer = `The factorial of ${number} is 1.`;
-        console.log(answer);
-        document.querySelector('#h4').innerHTML = answer;
     }
 
     // if number is positive
+
     else {
         let fact = 1;
         for (let i = 1; i <= number; i++) {
             fact *= i;
         }
         answer = `The factorial of ${number} is ${fact}.`;
-        console.log(answer);
-        document.querySelector('#h4').innerHTML = answer;
+
     }
+
+    console.log(answer);
+    document.querySelector('#h4').innerHTML = answer;
 }
